c35eee7ec3c9d7fe737030a5491e010bf15b4e68
SergioFernandez <sergiofernandezmuyor@gmail.com>
2025-06-25 22:26:24 +0200

    Muerte del jugador y mejora de cámara

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -320.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
